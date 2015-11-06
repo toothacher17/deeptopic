fbaeee69da490bfead83372452d612117cff6a7b
toothacher17 <toothacher17@gmail.com>
2015-11-05 22:54:36 -0500

    simplify data preparation

--- src/sampler/data/runner.sh
+++ src/sampler/data/runner.sh
@@ -6,13 +6,14 @@ g++ -O3 -std=c++11 preprocessing.cpp -o "fp"
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nips.txt.gz
 gunzip docword.nips.txt.gz
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nips.txt
-./fp docword.nips.txt vocab.nips.txt nips 500 0
+./fp docword.nips.txt vocab.nips.txt nips 500 1
 rm -rf docword.nips.txt vocab.nips.txt
 
-wget
-https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.enron.txt.gz
+#Download ENRON from UCI repository
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.enron.txt.gz
 gunzip docword.enron.txt.gz
-wget
-https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.enron.txt
-./fp docword.enron.txt vocab.enron.txt enron 500 0
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.enron.txt
+./fp docword.enron.txt vocab.enron.txt enron 500 1
 rm -rf docword.enron.txt.gz docword.enron.txt vocab.enron.txt
+
+rm ./fp
