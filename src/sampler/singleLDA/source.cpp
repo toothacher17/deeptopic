a75e409224c0ceb88837c53d10485216ce2aa566
toothacher17 <toothacher17@gmail.com>
2015-11-08 02:14:12 -0500

    finish debug matrix init

--- src/sampler/singleLDA/source.cpp
+++ src/sampler/singleLDA/source.cpp
@@ -6,20 +6,22 @@
 
 /* initialize the alpha matrix */
 // input is the matrix ptr, doc size M, topic size K
-void init_alpha(double ** alpha_mk, int M, int K) {
-    alpha_mk = new double*[M];
+double** init_alpha(int M, int K) {
+    double ** alpha_mk = new double* [M];
     for (int m = 0; m < M; m++) {
-        alpha_mk[m]  =new double[K];
+        alpha_mk[m] = new double[K];
         for (int k = 0; k < K; k++) {
             alpha_mk[m][k] = 5.0;
         }
     }
+    return alpha_mk;
 }
 
 
 /* free the alpha matrix */
-void free_alpha(double ** alpha_mk, int M) {
+void free_alpha(double *** alpha_mk_ptr, int M) {
     // size M K
+    double ** alpha_mk = *alpha_mk_ptr;
     if (alpha_mk) {
         for (int m = 0; m < M; m++) {
             if (alpha_mk[m]) {
@@ -39,11 +41,13 @@ int main(int argc, char ** argv)
     double ** alpha_mk;
     int M = 1000; // training data size
     int K = 10;   // topic size
-    init_alpha(alpha_mk, M, K);
-    std::cout << alpha_mk[2][3]<< std::endl;
-    std::cout << alpha_mk[902][3]<< std::endl;
+    alpha_mk = init_alpha(M, K);
+    std::cout << M << std::endl;
+    std::cout << K << std::endl;
+    std::cout << alpha_mk[2][3] << std::endl;
+    std::cout << alpha_mk[902][3] << std::endl;
 
-    free_alpha(alpha_mk, M);
+    free_alpha(&alpha_mk, M);
 
     /*
     model *lda=NULL;
